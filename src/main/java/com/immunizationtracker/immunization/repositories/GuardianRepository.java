e20febe4d0f66d329aa4b25e85b21cd4b73f5bb1
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 11:08:45 -0400

    Create GuardianRepository dao

---
+++ src/main/java/com/immunizationtracker/immunization/repositories/GuardianRepository.java
@@ -0,0 +1,8 @@
+package com.immunizationtracker.immunization.repositories;
+
+import com.immunizationtracker.immunization.models.Guardian;
+import org.springframework.data.repository.CrudRepository;
+
+public interface GuardianRepository extends CrudRepository<Guardian, Long>
+{
+}
