35f4c246b6a8a2e287722d7b9781cfe292283ea2
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 11:32:07 -0400

    Create GuardianService interface

---
+++ src/main/java/com/immunizationtracker/immunization/service/GuardianService.java
@@ -0,0 +1,19 @@
+package com.immunizationtracker.immunization.service;
+
+import com.immunizationtracker.immunization.models.Guardian;
+
+import java.util.List;
+
+public interface GuardianService
+{
+    List<Guardian> findAll();
+
+    Guardian findGuardianById(long id);
+
+    void delete(long id);
+
+    Guardian save(Guardian guardian);
+
+    Guardian update(Guardian guardian, long id);
+}
+ 
