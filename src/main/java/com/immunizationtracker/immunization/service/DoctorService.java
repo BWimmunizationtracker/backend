a140e36bbc959b4f89ed5d8f4192acb0a3b89c49
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 13:36:58 -0400

    Create DoctorService

---
+++ src/main/java/com/immunizationtracker/immunization/service/DoctorService.java
@@ -0,0 +1,19 @@
+package com.immunizationtracker.immunization.service;
+
+import com.immunizationtracker.immunization.models.Doctor;
+import com.immunizationtracker.immunization.models.Ward;
+
+import java.util.List;
+
+public interface DoctorService
+{
+    List<Doctor> findAll();
+
+    Doctor findDoctorById(long id);
+
+    void delete(long id);
+
+    Doctor save(Doctor doctor);
+
+    Doctor update(Doctor doctor, long id);
+}
