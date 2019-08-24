0cda2ed488390f5863abe32584df6f7b8caba73a
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 13:14:19 -0400

    Create WardService interface

---
+++ src/main/java/com/immunizationtracker/immunization/service/WardService.java
@@ -0,0 +1,19 @@
+package com.immunizationtracker.immunization.service;
+
+import com.immunizationtracker.immunization.models.Guardian;
+import com.immunizationtracker.immunization.models.Ward;
+
+import java.util.List;
+
+public interface WardService
+{
+    List<Ward> findAll();
+
+    Ward findWardById(long id);
+
+    void delete(long id);
+
+    Ward save(Ward ward);
+
+    Ward update(Ward ward, long id);
+}
