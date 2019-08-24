3e973a17dad4bb8e4336da902fde47105024b06c
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 00:01:06 -0400

    Create Immunization model

---
+++ src/main/java/com/immunizationtracker/immunization/models/Immunization.java
@@ -0,0 +1,68 @@
+package com.immunizationtracker.immunization.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "immunizations")
+public class Immunization extends Auditable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long immunizationid;
+
+    private Date date;
+    private String immunizationname;
+    private String clinic;
+
+    public Immunization()
+    {
+    }
+
+    public Immunization(String immunizationname, String clinic)
+    {
+        this.date = new Date();
+        this.immunizationname = immunizationname;
+        this.clinic = clinic;
+    }
+
+    public long getImmunizationid()
+    {
+        return immunizationid;
+    }
+
+    public void setImmunizationid(long immunizationid)
+    {
+        this.immunizationid = immunizationid;
+    }
+
+    public Date getDate()
+    {
+        return date;
+    }
+
+    public void setDate(Date date)
+    {
+        this.date = date;
+    }
+
+    public String getImmunizationname()
+    {
+        return immunizationname;
+    }
+
+    public void setImmunizationname(String immunizationname)
+    {
+        this.immunizationname = immunizationname;
+    }
+
+    public String getClinic()
+    {
+        return clinic;
+    }
+
+    public void setClinic(String clinic)
+    {
+        this.clinic = clinic;
+    }
+}
