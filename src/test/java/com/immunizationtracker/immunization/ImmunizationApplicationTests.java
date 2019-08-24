88ab6bdddd8f6dddb6b83c3ffe7681ca40c57b9e
Rory Murray <rory.christian.murray@gmail.com>
2019-08-23 20:36:51 -0400

    initial commit with pom.xml specified

---
+++ src/test/java/com/immunizationtracker/immunization/ImmunizationApplicationTests.java
@@ -0,0 +1,18 @@
+package com.immunizationtracker.immunization;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImmunizationApplicationTests
+{
+
+    @Test
+    public void contextLoads()
+    {
+    }
+
+}
