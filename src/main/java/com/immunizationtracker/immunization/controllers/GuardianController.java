168fd704e1b744cbbeb7b3348c90306fa416ec69
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 12:30:18 -0400

    Create delete guardian by id endpoint

--- src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
+++ src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
@@ -9,10 +9,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
@@ -50,6 +47,15 @@ public class GuardianController
     }
 
     // delete Guardian by id
+    @DeleteMapping("/guardian/{guardianid}")
+    public ResponseEntity<?> deleteGuardianById(@PathVariable long guardianid, HttpServletRequest request)
+    {
+        logger.info(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
+        guardianService.delete(guardianid);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
 
 
 
