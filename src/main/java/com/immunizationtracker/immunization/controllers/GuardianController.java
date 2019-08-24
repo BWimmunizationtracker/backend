f12fdd0bdf5d11d9dcc1d39f8e72f65b0bc3d1e1
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 11:46:59 -0400

    Add override methods and findAll method to GuardianServiceImpl

--- src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
+++ src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
@@ -2,6 +2,7 @@ package com.immunizationtracker.immunization.controllers;
 
 
 
+import com.immunizationtracker.immunization.service.GuardianService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +15,7 @@ public class GuardianController
 {
     private static final Logger logger = LoggerFactory.getLogger(GuardianController.class);
 
-    @Autowired
-//    private GuardianService guardianService;
 
+    @Autowired
+    private GuardianService guardianService;
 }
--- src/main/java/com/immunizationtracker/immunization/service/GuardianServiceImpl.java
+++ src/main/java/com/immunizationtracker/immunization/service/GuardianServiceImpl.java
@@ -1,5 +1,51 @@
 package com.immunizationtracker.immunization.service;
 
-public class GuardianServiceImpl
+import com.immunizationtracker.immunization.models.Guardian;
+import com.immunizationtracker.immunization.repositories.GuardianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service(value = "guardianService")
+public class GuardianServiceImpl implements GuardianService
 {
+    // We instantiate an instance of the GuardianRepository here
+    // so that we have access to the data access object methods
+    @Autowired
+    private GuardianRepository guardianRepository;
+
+    // next we will implement the methods from our interface
+    @Override
+    public List<Guardian> findAll()
+    {
+        List<Guardian> list = new ArrayList();
+        guardianRepository.findAll().iterator().forEachRemaining(list::add);
+        return list;
+    }
+
+    @Override
+    public Guardian findGuardianById(long id)
+    {
+        return null;
+    }
+
+    @Override
+    public void delete(long id)
+    {
+
+    }
+
+    @Override
+    public Guardian save(Guardian guardian)
+    {
+        return null;
+    }
+
+    @Override
+    public Guardian update(Guardian guardian, long id)
+    {
+        return null;
+    }
 }
