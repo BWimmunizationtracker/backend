83f5815269cdd81219daa127b4979627beb8239e
Rory Murray <rory.christian.murray@gmail.com>
2019-08-24 11:57:32 -0400

    Create listAllGuardians endpoint

--- src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
+++ src/main/java/com/immunizationtracker/immunization/controllers/GuardianController.java
@@ -2,12 +2,19 @@ package com.immunizationtracker.immunization.controllers;
 
 
 
+import com.immunizationtracker.immunization.models.Guardian;
 import com.immunizationtracker.immunization.service.GuardianService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("/guardians")
@@ -18,4 +25,18 @@ public class GuardianController
 
     @Autowired
     private GuardianService guardianService;
+
+    // get all Guardians
+    @GetMapping(value = "/allguardians", produces = {"application/json"})
+    public ResponseEntity<?> listAllGuardians(HttpServletRequest request)
+    {
+        logger.info(request.getMethod() + " " + request.getRequestURI() + " accessed");
+
+        List<Guardian> allGuardians = guardianService.findAll();
+        return new ResponseEntity<>(allGuardians, HttpStatus.OK);
+
+    }
+
+
+
 }
