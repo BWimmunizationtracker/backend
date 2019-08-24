0c455e6712b283fb4412f909afe5f260e4faea25
Rory Murray <rory.christian.murray@gmail.com>
2019-08-23 23:30:09 -0400

    Create Guardian model outline

---
+++ src/main/java/com/immunizationtracker/immunization/models/Guardian.java
@@ -0,0 +1,70 @@
+package com.immunizationtracker.immunization.models;
+
+import org.springframework.data.annotation.Id;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "guardians")
+public class Guardian extends Auditable
+{
+    // fields - state - values
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long parentid;
+
+    private String firstname;
+    private String lastname;
+//    private List<Doctor> gavePermissionDoctor = new ArrayList<>();
+//    private List<Child> children = new ArrayList<>();
+
+    // create base constructor
+
+    public Guardian()
+    {
+    }
+
+    // create constructor
+
+
+    public Guardian(String firstname, String lastname)
+    {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
+
+    // getter and setters
+    
+    public long getParentid()
+    {
+        return parentid;
+    }
+
+    public void setParentid(long parentid)
+    {
+        this.parentid = parentid;
+    }
+
+    public String getFirstname()
+    {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname)
+    {
+        this.firstname = firstname;
+    }
+
+    public String getLastname()
+    {
+        return lastname;
+    }
+
+    public void setLastname(String lastname)
+    {
+        this.lastname = lastname;
+    }
+}
