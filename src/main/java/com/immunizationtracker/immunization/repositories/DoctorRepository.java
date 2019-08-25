ebaf0db0194da656957cfc66a93be92081d83e59
Rory Murray <rory.christian.murray@gmail.com>
2019-08-25 18:39:27 -0400

    Create deletePermissionsByGuardianId method in DoctorRepository

--- src/main/java/com/immunizationtracker/immunization/repositories/DoctorRepository.java
+++ src/main/java/com/immunizationtracker/immunization/repositories/DoctorRepository.java
@@ -2,8 +2,17 @@ package com.immunizationtracker.immunization.repositories;
 
 import com.immunizationtracker.immunization.models.Doctor;
 import com.immunizationtracker.immunization.models.Guardian;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface DoctorRepository extends CrudRepository<Doctor, Long>
 {
+    @Transactional
+    @Modifying
+    @Query(value = "DELETE from permissions where guardianid = :guardianid")
+    void deletePermissionsByGuardianId( long guardianid);
+
+
 }
